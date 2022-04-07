445a9cd163e8a15a7e1ecc8b512161880c660114
Corey Schadeck <corey@schadeck.com>
2022-04-07 09:49:04 -0400

    commit tree 1ea37efd40e611c7d61960c0c322b501c413777e

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!" .toUpperCase() // 'HELLO'
+function whisper(string) {
+    return string.toLowerCase();
+}
+"Hello!" .toLowerCase()
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+"Hello" .toUpperCase();
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string) {
+    const string = "Hello!";
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
+}
